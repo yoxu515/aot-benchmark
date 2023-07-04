4780158eebe343652d7a2b8a13675fda8660ce65
Zongxin Yang <57658428+z-x-yang@users.noreply.github.com>
2023-07-04 23:02:25 +0800

    Update swinb_deaotl.py

--- configs/models/swinb_deaotl.py
+++ configs/models/swinb_deaotl.py
@@ -7,6 +7,7 @@ class ModelConfig(DefaultModelConfig):
         self.MODEL_NAME = 'SwinB_DeAOTL'
 
         self.MODEL_ENCODER = 'swin_base'
+        self.MODEL_ENCODER_PRETRAIN = './pretrain_models/swin_base_patch4_window7_224_22k.pth'  # https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224_22k.pth
         self.MODEL_ALIGN_CORNERS = False
         self.MODEL_ENCODER_DIM = [128, 256, 512, 512]  # 4x, 8x, 16x, 16x
 
