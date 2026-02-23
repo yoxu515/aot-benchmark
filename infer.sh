1e2fa340d33ecbb397fa3a389d9fea91635baa97
varun sai <varunsaip2005@gmail.com>
2026-02-23 10:42:33 +0530

    Added infer.sh

--- infer.sh
+++ infer.sh
@@ -1,13 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 ## Inference ##
 
-exp = "deafult"
-data_path = "./datasets/Demo"
-model = "r50_aotl"
-ckpt_path = "./pretrained_models/R50_AOTL_PRE_YTB_DAV.pth"
+stage="pre_ytb_dav"
+data_path="./datasets/Demo"
+model="r50_aotl"
+ckpt_path="./pretrain_models/R50_AOTL_PRE_YTB_DAV.pth"
 
 python tools/demo.py --amp \
-    --stage pre_ytb_dav \
-    --model ${model} \
-    --data_path ${data_path} \
-    --ckpt_path ${ckpt_path} \
-
+    --stage "${stage}" \
+    --model "${model}" \
+    --data_path "${data_path}" \
+    --ckpt_path "${ckpt_path}"
